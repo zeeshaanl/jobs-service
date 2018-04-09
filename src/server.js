2f636b05234640d4363cae80fc695906ec24d85d
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2018-04-10 00:22:59 +0200

    adjust build scripts

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import logger from './src/lib/logger';
+import logger from './lib/logger';
 
-import routes from './src/infrastructure/rest/routes'
+import routes from './infrastructure/rest/routes'
 
 const app = express();
 
@@ -13,9 +13,9 @@ app.use(morgan('combined', { stream: logger.stream }));
 app.use(express.json());
 app.use(express.urlencoded());
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/src/infrastructure/web/views');
+app.set('views', __dirname + '/infrastructure/web/views');
 
-app.use("/public", express.static(__dirname + '/src/infrastructure/web/public'));
+app.use("/public", express.static(__dirname + '/infrastructure/web/public'));
 
 // app.use(express.static(__dirname + '/src/infrastructure/web/public'));
 app.use(routes);
