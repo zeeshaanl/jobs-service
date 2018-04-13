1304344c4d6120214cf68681d589ac1d8e965176
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2018-04-13 17:11:13 +0200

    create build pipeline

--- cron/ImportJobs.js
+++ cron/ImportJobs.js
@@ -3,23 +3,33 @@ import appContainerInstance from '../src/AppContainer';
 import XingApiServiceFilesystemMock from '../test/unit/mock/XingApiServiceFilesystemMock';
 import XingImplementation from '../src/infrastructure/provider/Xing/XingImplementation';
 import XingApiService from '../src/infrastructure/provider/Xing/XingApiService';
-import WorkingNomadsApiServiceFilesystemMock from '../test/unit/mock/WorkingNomadsApiServiceFilesystemMock';
-import WorkingNomadsImplementation from '../src/infrastructure/provider/WorkingNomads/WorkingNomadsImplementation';
+import logger from '../dist/src/lib/logger';
+// import WorkingNomadsApiServiceFilesystemMock from '../test/unit/mock/WorkingNomadsApiServiceFilesystemMock';
+// import WorkingNomadsImplementation from '../src/infrastructure/provider/WorkingNomads/WorkingNomadsImplementation';
+// import WorkingNomadsApiService from '../src/infrastructure/provider/WorkingNomads/WorkingNomadsApiService';
 
 raven.config(process.env.SENTRY_URL).install();
 
 try {
+    logger.log({
+        level: 'info',
+        message: 'Cron Job started'
+    });
     const xingApiService = new XingApiService;
     const xingImplementation = new XingImplementation(xingApiService);
 
-    const workingNomadsApiService = new WorkingNomadsApiServiceFilesystemMock;
-    const workingNomadsImplementation = new WorkingNomadsImplementation(workingNomadsApiService);
+    // const workingNomadsApiService = new WorkingNomadsApiService();
+    // const workingNomadsImplementation = new WorkingNomadsImplementation(workingNomadsApiService);
 
-    const providers = [xingImplementation, workingNomadsImplementation];
+    const providers = [xingImplementation, /*workingNomadsImplementation*/];
 
     appContainerInstance.importJobsUseCase.invoke(providers);
 
 } catch (e) {
     console.log(e, 'ERROR IN CRON');
+    logger.log({
+        level: 'cron error',
+        message: e
+    });
     raven.captureException(e);
 }
--- src/infrastructure/provider/WorkingNomads/WorkingNomadsApiService.js
+++ src/infrastructure/provider/WorkingNomads/WorkingNomadsApiService.js
@@ -1,5 +1,5 @@
 export default class WorkingNomadsApiService {
     async getRawJobsString() {
-
+        throw Error('WorkingNomads Api Service not yet implemented')
     }
 }
